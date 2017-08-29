53ce6354e9be04654f8356fe2f4ef698cf629f59
Eric <Natelsoneric@gmail.com>
2017-08-29 11:50:06 -0400

    Updated sitemap

---
+++ js/testapp.js
@@ -0,0 +1,14 @@
+
+<script src="https://www.gstatic.com/firebasejs/4.3.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAJEcB1q-qcu8mlnV4GcUm66_y43cjSToQ",
+    authDomain: "amdatabase-e0c0f.firebaseapp.com",
+    databaseURL: "https://amdatabase-e0c0f.firebaseio.com",
+    projectId: "amdatabase-e0c0f",
+    storageBucket: "amdatabase-e0c0f.appspot.com",
+    messagingSenderId: "788016927591"
+  };
+  firebase.initializeApp(config);
+</script>
